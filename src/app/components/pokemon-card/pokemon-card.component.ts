c2a96ad74b018e3da2c2c6c4e2800e384516fbe2
LeonardoFMiranda <leomiranda1997@gmail.com>
2025-06-21 15:32:23 -0300

    Adiciona função para colorir background dos tipos Implementa fetch da PokéAPI para lista de pokémons Corrige CSS para responsividade web e mobile Melhora estrutura HTML da lista de pokémons

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,IonCardContent, IonImg } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonImg } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -23,5 +23,30 @@ export class PokemonCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  getTypeColor(typeName: string): string {
+    const typeColors: { [key: string]: string } = {
+      normal: '#dcdddd',
+      fire: '#ff6900',
+      water: '#15b9fe',
+      grass: '#b4f000',
+      electric: '#ffe100',
+      ice: '#15f4ff',
+      fighting: '#dc6900',
+      poison: '#d28dd3',
+      ground: '#fbc85b',
+      flying: '#78dcff',
+      psychic: '#f08cdc',
+      bug: '#46c846',
+      rock: '#b48c64',
+      ghost: '#a08cff',
+      dragon: '#5078dc',
+      dark: '#787878',
+      steel: '#aac8f0',
+      fairy: '#ffafdc'
+    };
+
+    return typeColors[typeName] || '#ccc'; // Cor padrão caso o tipo não exista
+  }
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,38 +10,187 @@ import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.co
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [
-    CommonModule, 
+    CommonModule,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
-    PokemonCardComponent
+    PokemonCardComponent,
+    HttpClientModule
   ],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  constructor() { }
 
-  pokemons = [
-    {
-      id: 1,
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-      forms: [{ name: 'bulbasaur' }],
-      types: [
-        { type: { name: 'grass' } },
-        { type: { name: 'poison' } }
-      ]
-    },
-    {
-      id: 4,
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-      forms: [{ name: 'charmander' }],
-      types: [
-        { type: { name: 'fire' } }
-      ]
-    }
-  ];
+  httpClient = inject(HttpClient);
+
+
+  //   {
+  //     id: 1,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  //     forms: [{ name: 'bulbasaur' }],
+  //     types: [
+  //       { type: { name: 'grass' } },
+  //       { type: { name: 'poison' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 4,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+  //     forms: [{ name: 'charmander' }],
+  //     types: [
+  //       { type: { name: 'fire' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 7,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
+  //     forms: [{ name: 'squirtle' }],
+  //     types: [
+  //       { type: { name: 'water' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 25,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+  //     forms: [{ name: 'pikachu' }],
+  //     types: [
+  //       { type: { name: 'electric' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 124,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/124.png',
+  //     forms: [{ name: 'jynx' }],
+  //     types: [
+  //       { type: { name: 'ice' } },
+  //       { type: { name: 'psychic' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 66,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/66.png',
+  //     forms: [{ name: 'machop' }],
+  //     types: [
+  //       { type: { name: 'fighting' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 23,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png',
+  //     forms: [{ name: 'ekans' }],
+  //     types: [
+  //       { type: { name: 'poison' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 104,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/104.png',
+  //     forms: [{ name: 'cubone' }],
+  //     types: [
+  //       { type: { name: 'ground' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 41,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/41.png',
+  //     forms: [{ name: 'zubat' }],
+  //     types: [
+  //       { type: { name: 'poison' } },
+  //       { type: { name: 'flying' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 63,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/63.png',
+  //     forms: [{ name: 'abra' }],
+  //     types: [
+  //       { type: { name: 'psychic' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 10,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png',
+  //     forms: [{ name: 'caterpie' }],
+  //     types: [
+  //       { type: { name: 'bug' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 95,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/95.png',
+  //     forms: [{ name: 'onix' }],
+  //     types: [
+  //       { type: { name: 'rock' } },
+  //       { type: { name: 'ground' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 92,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png',
+  //     forms: [{ name: 'gastly' }],
+  //     types: [
+  //       { type: { name: 'ghost' } },
+  //       { type: { name: 'poison' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 147,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/147.png',
+  //     forms: [{ name: 'dratini' }],
+  //     types: [
+  //       { type: { name: 'dragon' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 198,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/198.png',
+  //     forms: [{ name: 'murkrow' }],
+  //     types: [
+  //       { type: { name: 'dark' } },
+  //       { type: { name: 'flying' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 81,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/81.png',
+  //     forms: [{ name: 'magnemite' }],
+  //     types: [
+  //       { type: { name: 'electric' } },
+  //       { type: { name: 'steel' } }
+  //     ]
+  //   },
+  //   {
+  //     id: 35,
+  //     image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png',
+  //     forms: [{ name: 'clefairy' }],
+  //     types: [
+  //       { type: { name: 'fairy' } }
+  //     ]
+  //   }
+  // ];
+  pokemons: any[] = [];
+
 
   ngOnInit() {
     console.log(this.pokemons);
+    this.fetchPokemons();
+  }
+
+  fetchPokemons() {
+    this.httpClient
+      .get('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .subscribe((response: any) => {
+        response.results.forEach((pokemon: any, index: number) => {
+          this.httpClient.get(pokemon.url).subscribe((pokemonDetails: any) => {
+            this.pokemons.push({
+              id: pokemonDetails.id,
+              image: pokemonDetails.sprites.front_default,
+              forms: [{ name: pokemonDetails.name }],
+              types: pokemonDetails.types
+            });
+          });
+        });
+      });
   }
 }
